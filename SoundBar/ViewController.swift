babfe02897252816d2f31a31cb4fb476aadc75c1
Mariam Dalia Apaza Santillana <mariam.apaza@tecsup.edu.pe>
2021-05-25 09:34:49 -0700

    Initial Commit

---
+++ SoundBar/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SoundBar
+//
+//  Created by Mariam Apaza on 5/25/21.
+//  Copyright © 2021 Mariam Apaza. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
